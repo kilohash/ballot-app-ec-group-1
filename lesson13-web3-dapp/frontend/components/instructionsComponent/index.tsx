a55d359d589b03c1e13dbab5147e7b81949bdb11
kilohash <r-type@ukr.net>
2023-08-28 21:04:57 +0300

    Lesson 16. Oracles

--- lesson13-web3-dapp/frontend/components/instructionsComponent/index.tsx
+++ lesson13-web3-dapp/frontend/components/instructionsComponent/index.tsx
@@ -1,4 +1,7 @@
+import { useEffect, useState } from "react";
 import styles from "./instructionsComponent.module.css";
+import { useBalance, useSignMessage } from "wagmi";
+// import { setData } 
 
 export default function InstructionsComponent() {
   return (
@@ -18,12 +21,94 @@ export default function InstructionsComponent() {
   )
 };
 
+useEffect(() => {
+  fetch("http://localhost:3001/get-address")
+    .then((res) => res.json())
+    .then((data) => {
+      setData(data);
+      setLoading(false);
+    });
+}, []);
+
+function WalletBalance(params: { address: `0x${string}`}) {
+  const { data, isError, isLoading } = useBalance({
+    address: params.address,
+  });
+}
+
+function WalletAction() {
+  const [signatureMessage, setSignatureMessage] = useState("My Input Value");
+
+  const { data, isError, isLoading, isSuccess, signMessage } = useSignMessage();
+  return (
+    <div>
+      <form>
+        <label>
+          Enter the message to be signed:
+          <input
+            type="text"
+            value={signatureMessage}
+            onChange={(e) => setSignatureMessage(e.target.value)}
+            />
+        </label>
+      </form>
+      <button
+        disabled={isLoading}
+        onClick={() =>
+        signMessage({
+          message: signatureMessage,
+        })}
+      ></button>
+      )
+    </div>
+  );
+}
+
 function PageBody() {
   const a = Math.random();
-  return ( 
+  return (
   <div>
-    <p>Something Static</p>
-    <p>Something Dynamic: {a}</p>
+    <WalletInfo></WalletInfo>
+    <RandomFile></RandomFile>
+    <></>
   </div>
   );
+}
+
+function RequestTokensToBeMinted(params: { address: string}) {
+  const [data, setData] = useState<any>(null);
+  const [isLoading, setLoading] = useState(false);
+
+  if (isLoading) return <p>Requesting tokens from API...</p>;
+
+  const requestOptions = {
+    method: "POST",
+    Headers: { "Content-Type": "application/json"},
+    body: JSON.stringify({address: params.address}),
+  };
+  
+  if (!data)
+  return (
+    <button
+      disabled={isLoading}
+      onClick={() => {
+        setLoading(true);
+        fetch("http://localhost:3001/mint-tokens", requestOptions)
+          .then((res) => res.json())
+          .then((data) => {
+            setData(data);
+            setLoading(false);
+          });
+      }}
+    >
+      Request Tokens
+    </button>
+  );
+  
+  return (
+    <div>
+      <p> Mint success: {data.success ? "worked" : "failed"} </p>
+      <p> Transaction hash: {data.txHash} </p>
+    </div>
+    );
 }
